00ed7e485aef3f3b70764ee380f2f6074f54863a
kmazia <80555230+kmazia@users.noreply.github.com>
2021-09-21 20:52:03 -0300

    Add files via upload

---
+++ aula11/index.js
@@ -0,0 +1,24 @@
+
+
+let lista = ["Feijão", "Sabonete", "Arroz", "Escova", "Farinha"]
+ 
+
+console.log(lista.join('e'));
+
+console.log(lista.splice(0, 1));
+
+console.log(litsa[0].split(''));
+
+console.log(lista.pop())
+
+
+//console.log("O método Join junta índices de um array, podendo colocar um caractere entre cada índice.Ex: " + lista.join(" - "))
+//console.log("O método Pop remove e retorna o último índice do array. Ex: " + lista.pop())
+ 
+//lista.push("Final")
+//console.log("O método Push adiciona um novo valor ao array, no seu final. Ex: " + lista)
+ 
+//console.log("O método Shift remove e retorna o primeiro elemento do array. Ex: " + lista.shift())
+ 
+//lista.unshift("Começo")
+//console.log("O método Push adiciona um novo valor ao array, no seu começo. Ex: " + lista)
